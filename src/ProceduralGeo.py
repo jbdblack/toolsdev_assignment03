2bcf405655bc57f484e8a8066d2039521dc71a14
jbdblack <65990035+jbdblack@users.noreply.github.com>
2020-07-19 15:05:38 -0500

    Added sliders for brick attributes

--- src/ProceduralGeo.py
+++ src/ProceduralGeo.py
@@ -33,25 +33,67 @@ class SliderBox(QtWidgets.QAbstractSlider):
         self.createbricks()
 
     def create_widgets(self):
-        self.ext_lbl = QtWidgets.QLabel()
-        self.sld_lbl_01 = QtWidgets.QLabel()
-        self.sld_lbl_02 = QtWidgets.QLabel()
-        self.ext_sld_01 = QtWidgets.QSlider()
-        self.ext_sld_02 = QtWidgets.QSlider()
-        self.ext_sld_01.setOrientation(Qt.Horizontal)
-        self.ext_sld_02.setOrientation(Qt.Horizontal)
-        self.ext_sld_01.setMaximum(100)
-        self.ext_sld_02.setMaximum(100)
+        """Creates widgets for our UI"""
+        self.sld_lbl_row = QtWidgets.QLabel()
+        self.sld_lbl_col = QtWidgets.QLabel()
+        self.sld_lbl_bvl = QtWidgets.QLabel()
+        self.sld_lbl_offset = QtWidgets.QLabel()
+        self.sld_lbl_width = QtWidgets.QLabel()
+        self.sld_lbl_height = QtWidgets.QLabel()
+        self.sld_lbl_depth = QtWidgets.QLabel()
+
+        self.ext_sld_row = QtWidgets.QSlider()
+        self.ext_sld_col = QtWidgets.QSlider()
+        self.ext_sld_bvl = QtWidgets.QSlider()
+        self.ext_sld_offset = QtWidgets.QSlider()
+        self.ext_sld_width = QtWidgets.QSlider()
+        self.ext_sld_height = QtWidgets.QSlider()
+        self.ext_sld_depth = QtWidgets.QSlider()
+
+        self.ext_sld_row.setOrientation(Qt.Horizontal)
+        self.ext_sld_col.setOrientation(Qt.Horizontal)
+        self.ext_sld_bvl.setOrientation(Qt.Horizontal)
+        self.ext_sld_offset.setOrientation(Qt.Horizontal)
+        self.ext_sld_width.setOrientation(Qt.Horizontal)
+        self.ext_sld_height.setOrientation(Qt.Horizontal)
+        self.ext_sld_depth.setOrientation(Qt.Horizontal)
+
+        self.ext_sld_row.setMaximum(250)
+        self.ext_sld_col.setMaximum(250)
+        self.ext_sld_bvl.setMaximum(1)
+        self.ext_sld_offset.setMaximum(0.4)
+        self.ext_sld_width.setMaximum(20)
+        self.ext_sld_width.setValue(2)
+        self.ext_sld_height.setMaximum(20)
+        self.ext_sld_height.setValue(1)
+        self.ext_sld_depth.setMaximum(20)
 
         self.brick_btn = QtWidgets.QPushButton("Create Bricks")
 
     def create_layout(self):
         """Lay out our widgets in the UI"""
         self.sliders_lay = QtWidgets.QFormLayout()
-        self.sliders_lay.addRow(self.tr("&Height:"), self.sld_lbl_01)
-        self.sliders_lay.addRow(self.ext_sld_01)
-        self.sliders_lay.addRow(self.tr("&Width:"), self.sld_lbl_02)
-        self.sliders_lay.addRow(self.ext_sld_02)
+        self.sliders_lay.addRow(self.tr("&Row:"), self.sld_lbl_row)
+        self.sliders_lay.addRow(self.ext_sld_row)
+        self.sliders_lay.addRow(self.tr("&Column:"), self.sld_lbl_col)
+        self.sliders_lay.addRow(self.ext_sld_col)
+
+        self.sliders_lay.addRow(self.tr("&Bevel:"), self.sld_lbl_bvl)
+        self.sliders_lay.addRow(self.ext_sld_bvl)
+
+        self.sliders_lay.addRow(self.tr("&Brick Offset:"), self.sld_lbl_offset)
+        self.sliders_lay.addRow(self.ext_sld_offset)
+
+        self.sliders_lay.addRow(self.tr("&Width:"), self.sld_lbl_width)
+        self.sliders_lay.addRow(self.ext_sld_width)
+
+        self.sliders_lay.addRow(self.tr("&Height:"), self.sld_lbl_height)
+        self.sliders_lay.addRow(self.ext_sld_height)
+
+        self.sliders_lay.addRow(self.tr("&Depth:"), self.sld_lbl_depth)
+        self.sliders_lay.addRow(self.ext_sld_depth)
+
+
 
         self.bottom_btn_lay = QtWidgets.QHBoxLayout()
         self.bottom_btn_lay.addWidget(self.brick_btn)
@@ -64,13 +106,18 @@ class SliderBox(QtWidgets.QAbstractSlider):
 
     def create_connections(self):
         """Connect our widget signal to slots"""
-        self.ext_sld_01.valueChanged.connect(self.sld_lbl_01.setNum)
-        self.ext_sld_02.valueChanged.connect(self.sld_lbl_02.setNum)
+        self.ext_sld_row.valueChanged.connect(self.sld_lbl_row.setNum)
+        self.ext_sld_col.valueChanged.connect(self.sld_lbl_col.setNum)
+        self.ext_sld_bvl.valueChanged.connect(self.sld_lbl_bvl.setNum)
+        self.ext_sld_offset.valueChanged.connect(self.sld_lbl_offset.setNum)
+        self.ext_sld_width.valueChanged.connect(self.sld_lbl_width.setNum)
+        self.ext_sld_height.valueChanged.connect(self.sld_lbl_height.setNum)
+        self.ext_sld_depth.valueChanged.connect(self.sld_lbl_depth.setNum)
         self.brick_btn.clicked.connect(self.createbricks)
 
 
     def slider_value(self):
-        slider_val_01 = self.ext_sld_01.value()
+        slider_val_01 = self.ext_sld_row.value()
         return slider_val_01
 
     @QtCore.Slot()
@@ -79,13 +126,13 @@ class SliderBox(QtWidgets.QAbstractSlider):
         offx = 0.0
         offy = 0.0
         offz = 0.0
-        row = int(self.ext_sld_01.value())
-        col = int(self.ext_sld_02.value())
-        bevel = float(1)
-        width = float(2)
-        height = float(2)
-        depth = float(2)
-        offset = float(2)
+        row = int(self.ext_sld_row.value())
+        col = int(self.ext_sld_col.value())
+        bevel = float(self.ext_sld_bvl.value())
+        width = float(self.ext_sld_width.value())
+        height = float(self.ext_sld_height.value())
+        depth = float(self.ext_sld_depth.value())
+        offset = float(self.ext_sld_offset.value())
 
         for i in range(0,row):
             for j in range(0,col):
